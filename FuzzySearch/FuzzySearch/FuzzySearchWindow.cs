fd3bd87597845c66301dc203a44aaee83ff971d7
Muhammad Hilman Beyri <hilmanbeyri@gmail.com>
2018-12-26 12:06:32 -0500

    updated vs manifest

--- FuzzySearch/FuzzySearch/FuzzySearchWindow.cs
+++ FuzzySearch/FuzzySearch/FuzzySearchWindow.cs
@@ -24,7 +24,7 @@
         /// </summary>
         public FuzzySearchWindow() : base(null)
         {
-            this.Caption = "FuzzySearchWindow";
+            this.Caption = "Fuzzy Search Window";
 
             // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
             // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
