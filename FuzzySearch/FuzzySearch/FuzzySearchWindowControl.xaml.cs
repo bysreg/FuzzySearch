2b12ebf1c8e2480877530e936ee0322a971643b0
Muhammad Hilman Beyri <hilmanbeyri@gmail.com>
2018-12-27 19:37:00 -0500

    added score for the fuzzy match

--- FuzzySearch/FuzzySearch/FuzzySearchWindowControl.xaml.cs
+++ FuzzySearch/FuzzySearch/FuzzySearchWindowControl.xaml.cs
@@ -75,24 +75,66 @@
             }
         }
 
-        bool FuzzyMatch(string search, string str)
+        // First element of the tuple would be true if search "matches" the str string
+        // Second element of the tuple would be the score of the "matchness"
+        Tuple<bool, int> FuzzyMatch(string search, string str)
         {
             int search_i = 0, search_len = search.Length;
-            int str_i = 0, full_path_len = str.Length;
+            int str_i = 0, str_len = str.Length;
 
-            if (search_len > full_path_len)
-                return false;
+            bool prev_matched = false;
+            int first_str_match = -1;
 
-            while (search_i < search_len && str_i < full_path_len)
+            int score = 0;
+
+            if (search_len > str_len)
+                return Tuple.Create(false, score);
+
+            while (search_i < search_len && str_i < str_len)
             {
                 if (search[search_i] == Char.ToLower(str[str_i]))
                 {
                     ++search_i;
+
+                    // consecutive matches worth more
+                    if (prev_matched)
+                        score += 5;
+
+                    prev_matched = true;
+
+                    if (first_str_match == -1)
+                        first_str_match = str_i;
+                }
+                else
+                {
+                    prev_matched = false;
                 }
                 ++str_i;
             }
 
-            return search_i == search_len;
+            // unmatched letter scores negatively (every unmatch letter is -1)
+            score -= (str_len - search_i);
+
+            // unmatched leading letter affect negatively (capped to -9)
+            score -= Math.Min(first_str_match * 3, 9);
+
+            return Tuple.Create(search_i == search_len, score);
+        }
+
+        struct SearchResult
+        {
+
+            public string FullPath { get; set; }
+            public string FileName { get; set; }
+
+            public int score;
+
+            public SearchResult(string full_path, string filename, int score)
+            {
+                FullPath = full_path;
+                FileName = filename;
+                this.score = score;
+            }
         }
 
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
@@ -104,18 +146,24 @@
             // - cache the previous search result, if a character is added to the 
             //   search query, then we only need to do fuzzy match against the
             //   the previous search result
-            List<WorkspaceFileInfo> results = new List<WorkspaceFileInfo>();
+            List<SearchResult> results = new List<SearchResult>();
             foreach (WorkspaceFileInfo file_info in FuzzySearchWindowCommand.Instance.WorkspaceFiles)
             {
-                // first, do fuzzy match agains the filename
-                // if it returns false, then we do fuzzy match against the full path
-                if (FuzzyMatch(search, file_info.filename) || FuzzyMatch(search, file_info.full_path))
+                // first, do fuzzy match agains the filename instead of the fullpath
+                Tuple<bool, int> result = FuzzyMatch(search, file_info.filename);
+                if (!result.Item1)
                 {
-                    results.Add(file_info);
+                    // if there's no match agains the filename, then we do fuzzy match against the full path
+                    result = FuzzyMatch(search, file_info.full_path);
                 }
+                if (result.Item1)
+                    results.Add(new SearchResult(file_info.full_path, file_info.filename, result.Item2));
             }
 
-            // todo: need to rank the results
+            results.Sort(delegate (SearchResult a, SearchResult b)
+            {
+                return b.score - a.score;
+            });
 
             // cap the result to the top 100
             if (results.Count > 100)
